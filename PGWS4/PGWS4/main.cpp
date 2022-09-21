aedb8e3ccd21c90246272e53881479898d3a608b
daichi0907 <daichi.watanabe0907@icloud.com>
2022-09-21 18:46:40 +0900

    第2回の授業内容を実装したプロジェクトファイルを追加しました。

---
+++ PGWS4/PGWS4/main.cpp
@@ -0,0 +1,253 @@
+﻿//ウィンドウ表示＆DirectX初期化
+#include<Windows.h>
+#include<tchar.h>
+#include<d3d12.h>
+#include<dxgi1_6.h>
+#include<vector>
+#include<string>
+#ifdef _DEBUG
+#include<iostream>
+#endif
+
+
+#pragma comment(lib,"d3d12.lib")
+#pragma comment(lib,"dxgi.lib")
+
+///@brief コンソール画面にフォーマット付き文字列を表示
+///@param format フォーマット(%dとか%fとかの)
+///@param 可変長引数
+///@remarksこの関数はデバッグ用です。デバッグ時にしか動作しません
+void DebugOutputFormatString(const char* format, ...) {
+#ifdef _DEBUG
+	va_list valist;
+	va_start(valist, format);
+	vprintf(format, valist);
+	va_end(valist);
+#endif
+}
+
+//面倒ですが、ウィンドウプロシージャは必須なので書いておきます
+LRESULT WindowProcedure(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
+	if (msg == WM_DESTROY) {//ウィンドウが破棄されたら呼ばれます
+		PostQuitMessage(0);//OSに対して「もうこのアプリは終わるんや」と伝える
+		return 0;
+	}
+	return DefWindowProc(hwnd, msg, wparam, lparam);//規定の処理を行う
+}
+
+
+#ifdef _DEBUG
+	int main() {
+#else
+	#include<Windows.h>
+	int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+#endif
+	const unsigned int window_width = 1280;
+	const unsigned int window_height = 720;
+
+	ID3D12Device* _dev = nullptr;
+	IDXGIFactory6* _dxgiFactory = nullptr;
+	ID3D12CommandAllocator* _cmdAllocator = nullptr;
+	ID3D12GraphicsCommandList* _cmdList = nullptr;
+	ID3D12CommandQueue* _cmdQueue = nullptr;
+	IDXGISwapChain4* _swapchain = nullptr;
+
+	HINSTANCE hInst = GetModuleHandle(nullptr);
+	//ウィンドウクラス生成＆登録
+	WNDCLASSEX w = {};
+	w.cbSize = sizeof(WNDCLASSEX);
+	w.lpfnWndProc = (WNDPROC)WindowProcedure;//コールバック関数の指定
+	w.lpszClassName = _T("DirectXTest");//アプリケーションクラス名(適当でいいです)
+	w.hInstance = GetModuleHandle(0);//ハンドルの取得
+	RegisterClassEx(&w);//アプリケーションクラス(こういうの作るからよろしくってOSに予告する)
+
+	RECT wrc = { 0,0, window_width, window_height };//ウィンドウサイズを決める
+	AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);//ウィンドウのサイズはちょっと面倒なので関数を使って補正する
+	//ウィンドウオブジェクトの生成
+	HWND hwnd = CreateWindow(w.lpszClassName,//クラス名指定
+		_T("DX12テスト"),//タイトルバーの文字
+		WS_OVERLAPPEDWINDOW,//タイトルバーと境界線があるウィンドウです
+		CW_USEDEFAULT,//表示X座標はOSにお任せします
+		CW_USEDEFAULT,//表示Y座標はOSにお任せします
+		wrc.right - wrc.left,//ウィンドウ幅
+		wrc.bottom - wrc.top,//ウィンドウ高
+		nullptr,//親ウィンドウハンドル
+		nullptr,//メニューハンドル
+		w.hInstance,//呼び出しアプリケーションハンドル
+		nullptr);//追加パラメータ
+
+	HRESULT D3D12CreateDevice(
+		IUnknown * pAdapter, // ひとまずは nullptr で OK
+		D3D_FEATURE_LEVEL MinimumFeatureLevel, // 最低限必要なフィーチャーレベル
+		REFIID riid, // 後述
+		void** ppDevice // 後述
+	);
+
+	HRESULT CreateSwapChainForHwnd(
+		IUnknown * pDevice, //コマンドキューオブジェクト
+		HWND hWnd, //ウィンドウハンドル
+		const DXGI_SWAP_CHAIN_DESC1 * pDesc, // スワップチェーン設定
+		const DXGI_SWAP_CHAIN_FULLSCREEN_DESC * pFullscreenDesc, // ひとまず nullptr でよい
+		IDXGIOutput * pRestrictToOutput, // これも nullptr でよい
+		IDXGISwapChain1 * *ppSwapChain // スワップチェーンオブジェクト取得用
+	);
+
+	D3D_FEATURE_LEVEL levels[] =
+	{
+		D3D_FEATURE_LEVEL_12_1,
+		D3D_FEATURE_LEVEL_12_0,
+		D3D_FEATURE_LEVEL_11_1,
+		D3D_FEATURE_LEVEL_11_0,
+	};
+
+	auto result = CreateDXGIFactory1(IID_PPV_ARGS(&_dxgiFactory));
+	// アダプターの列挙用
+	std::vector <IDXGIAdapter*> adapters;
+	// ここに特定の名前を持つアダプターオブジェクトが入る
+	IDXGIAdapter* tmpAdapter = nullptr;
+	for (int i = 0;
+		_dxgiFactory->EnumAdapters(i, &tmpAdapter) != DXGI_ERROR_NOT_FOUND;
+		++i)
+	{
+		adapters.push_back(tmpAdapter);
+	}
+	for (auto adpt : adapters)
+	{
+		DXGI_ADAPTER_DESC adesc = {};
+		adpt->GetDesc(&adesc); // アダプターの説明オブジェクト取得
+		std::wstring strDesc = adesc.Description;
+		// 探したいアダプターの名前を確認
+		if (strDesc.find(L"NVIDIA") != std::string::npos)
+		{
+			tmpAdapter = adpt;
+			break;
+		}
+	}
+
+	// Direct3D デバイスの初期化
+	D3D_FEATURE_LEVEL featureLevel;
+	for (auto lv : levels)
+	{
+		if (D3D12CreateDevice(nullptr, lv, IID_PPV_ARGS(&_dev)) == S_OK)
+		{
+			featureLevel = lv;
+			break; // 生成可能なバージョンが見つかったらループを打ち切り
+		}
+	}
+
+	result = _dev->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT,
+		IID_PPV_ARGS(&_cmdAllocator));
+	result = _dev->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_DIRECT,
+		_cmdAllocator, nullptr,
+		IID_PPV_ARGS(&_cmdList));
+
+	D3D12_COMMAND_QUEUE_DESC cmdQueueDesc = {};
+	// タイムアウト無し
+	cmdQueueDesc.Flags = D3D12_COMMAND_QUEUE_FLAG_NONE;
+	// アダプターを 1 つしか使わないときは 0 でよい
+	cmdQueueDesc.NodeMask = 0;
+	// プライオリティは特に指定なし
+	cmdQueueDesc.Priority = D3D12_COMMAND_QUEUE_PRIORITY_NORMAL;
+	// コマンドリストと合わせる
+	cmdQueueDesc.Type = D3D12_COMMAND_LIST_TYPE_DIRECT;
+	// キュー生成
+	result = _dev->CreateCommandQueue(&cmdQueueDesc, IID_PPV_ARGS(&_cmdQueue));
+
+	DXGI_SWAP_CHAIN_DESC1 swapchainDesc = {};
+	swapchainDesc.Width = window_width;
+	swapchainDesc.Height = window_height;
+	swapchainDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	swapchainDesc.Stereo = false;
+	swapchainDesc.SampleDesc.Count = 1;
+	swapchainDesc.SampleDesc.Quality = 0;
+	swapchainDesc.BufferUsage = DXGI_USAGE_BACK_BUFFER;
+	swapchainDesc.BufferCount = 2;
+	// バックバッファーは伸び縮み可能
+	swapchainDesc.Scaling = DXGI_SCALING_STRETCH;
+	// フリップ後は速やかに破棄
+	swapchainDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
+	// 特に指定なし
+	swapchainDesc.AlphaMode = DXGI_ALPHA_MODE_UNSPECIFIED;
+	// ウィンドウ⇔フルスクリーン切り替え可能
+	swapchainDesc.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;
+	result = _dxgiFactory->CreateSwapChainForHwnd(
+		_cmdQueue, hwnd,
+		&swapchainDesc, nullptr, nullptr,
+		(IDXGISwapChain1**)&_swapchain); //	本来はQueryInterface などを用いて
+										 // IDXGISwapChain4 への変数チェックをするが、
+										 //	ここではわかりやすさ重視のためキャストで対応
+
+	D3D12_DESCRIPTOR_HEAP_DESC heapDesc = {};
+	heapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV; // レンダーターゲットビューなので RTV
+	heapDesc.NodeMask = 0;
+	heapDesc.NumDescriptors = 2; // 表裏の 2 つ
+	heapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
+
+	ID3D12DescriptorHeap* rtvHeaps = nullptr;
+	result = _dev->CreateDescriptorHeap(&heapDesc, IID_PPV_ARGS(&rtvHeaps));
+
+	DXGI_SWAP_CHAIN_DESC swcDesc = {};
+	result = _swapchain->GetDesc(&swcDesc);
+
+	std::vector<ID3D12Resource*> _backBuffers(swcDesc.BufferCount);
+	for (int idx = 0; idx < swcDesc.BufferCount; ++idx)
+	{
+		result = _swapchain->GetBuffer(idx, IID_PPV_ARGS(&_backBuffers[idx]));
+		D3D12_CPU_DESCRIPTOR_HANDLE handle
+			= rtvHeaps->GetCPUDescriptorHandleForHeapStart();
+		handle.ptr += idx * _dev->GetDescriptorHandleIncrementSize(
+			D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
+		_dev->CreateRenderTargetView(_backBuffers[idx], nullptr, handle);
+	}
+
+	// ウィンドウ表示
+	ShowWindow(hwnd, SW_SHOW);
+
+	while (true)
+	{
+		MSG msg;
+		if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+		{
+			// アプリケーションが終わるときに message が WM_QUITになる
+			if (msg.message == WM_QUIT)
+			{
+				break;
+			}
+
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+
+		// DirectX処理
+		// バックバッファのインデックスを取得
+		auto bbIdx = _swapchain->GetCurrentBackBufferIndex();
+
+		// レンダーターゲットっを指定
+		auto rtvH = rtvHeaps->GetCPUDescriptorHandleForHeapStart();
+		rtvH.ptr += bbIdx * _dev->GetDescriptorHandleIncrementSize(
+			D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
+		_cmdList->OMSetRenderTargets(1, &rtvH, true, nullptr);
+
+		// 画面クリア
+		float clearColor[] = { 1.0f,1.0f,0.0f,1.0f }; // 黄色
+		_cmdList->ClearRenderTargetView(rtvH, clearColor, 0, nullptr);
+
+		// 命令のクローズ
+		_cmdList->Close();
+
+		// コマンドリストの実行
+		ID3D12CommandList* cmdlists[] = { _cmdList };
+		_cmdQueue->ExecuteCommandLists(1, cmdlists);
+
+		_cmdAllocator->Reset(); // キューをクリア
+		_cmdList->Reset(_cmdAllocator, nullptr); // 再びコマンドリストをためる準備
+
+		// フリップ
+		_swapchain->Present(1, 0);
+	}
+
+	// もうクラスは使わないので登録解除する
+	UnregisterClass(w.lpszClassName, w.hInstance);
+
+	return 0;
+}
