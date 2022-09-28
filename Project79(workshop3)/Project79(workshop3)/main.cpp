d1f8b415c9ee2c6efc15f690184ae17abbb299bb
omorir <g2027011@st.t-kougei.ac.jp>
2022-09-28 18:18:53 +0900

    slot

---
+++ Project79(workshop3)/Project79(workshop3)/main.cpp
@@ -0,0 +1,433 @@
+#include <Windows.h>
+#include <tchar.h>
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include <DirectXMath.h>
+#include<vector>
+#include<d3dcompiler.h>
+#ifdef  _DEBUG
+#include <iostream>
+#endif //  _DEBUG
+
+#pragma comment(lib, "d3d12.lib")
+#pragma comment(lib, "dxgi.lib")
+#pragma comment(lib, "d3dcompiler.lib")
+
+using namespace std;
+using namespace DirectX;
+
+XMFLOAT3 vertices[3];
+
+void DebugOutputFormatString(const char* format, ...)
+{
+#ifdef  _DEBUG
+	va_list valist;
+	va_start(valist, format);
+	printf(format, valist);
+	va_end(valist);
+#endif //  _DEBUG
+}
+
+
+LRESULT WindowsProcedure(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
+{
+	// ウィンドウが破棄されたら呼ばれる
+	if (msg == WM_DESTROY)
+	{
+		PostQuitMessage(0);
+		return 0;
+	}
+	return DefWindowProc(hwnd, msg, wparam, lparam);
+}
+
+#ifdef _DEBUG
+void EnableDebugLayer()
+{
+	ID3D12Debug* debugLayer = nullptr;
+	HRESULT result = D3D12GetDebugInterface(IID_PPV_ARGS(&debugLayer));
+	if (!SUCCEEDED(result)) return;
+
+	debugLayer->EnableDebugLayer();
+	debugLayer->Release();
+}
+#endif //_DEBUG
+
+#ifdef _DEBUG
+int main()
+{
+	//#else アクティブでないプリプロセッサ　ブロック
+	//#endif
+#ifdef _DEBUG
+	EnableDebugLayer();
+#endif // _DEBUG
+
+	const unsigned int window_width = 1280;
+	const unsigned int window_height = 720;
+	int FlameTime = 0;
+
+	ID3D12Device* _dev = nullptr;
+	IDXGIFactory6* _dxgiFactory = nullptr;
+	ID3D12CommandAllocator* _cmdAllocator = nullptr;
+	ID3D12GraphicsCommandList* _cmdList = nullptr;
+	ID3D12CommandQueue* _cmdQueue = nullptr;
+	IDXGISwapChain4* _swapchain = nullptr;
+
+	// ウィンドウクラスの生成&登録
+	WNDCLASSEX w = {};
+
+	w.cbSize = sizeof(WNDCLASSEX);
+	w.lpfnWndProc = (WNDPROC)WindowsProcedure; // コールバック関数の指定
+	w.lpszClassName = _T("DX12Sample"); // アプリケーションクラス名(適当でよい)
+	w.hInstance = GetModuleHandle(nullptr); // ハンドルの取得
+
+	RegisterClassEx(&w); // アプリケーションクラス(ウィンドウクラスの指定を OS に伝える)
+
+	RECT wrc = { 0, 0, window_width, window_height }; // ウィンドウサイズを決める
+
+	// 関数を使ってウィンドウのサイズを補正する
+	AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
+	// ウィンドウオブジェクトの生成
+	HWND hwnd = CreateWindow(w.lpszClassName, // クラス名指定
+		_T("DX12 テスト"), // タイトルバーの文字
+		WS_OVERLAPPEDWINDOW, // タイトルバーと境界線があるウィンドウ
+		CW_USEDEFAULT, // 表示 x 座標はOSにお任せ
+		CW_USEDEFAULT, // 表示 y 座標はOSにお任せ
+		wrc.right - wrc.left,
+		wrc.bottom - wrc.top,
+		nullptr,
+		nullptr,
+		w.hInstance,
+		nullptr);
+
+	D3D_FEATURE_LEVEL levels[] =
+	{
+		D3D_FEATURE_LEVEL_12_1,
+		D3D_FEATURE_LEVEL_12_0,
+		D3D_FEATURE_LEVEL_11_1,
+		D3D_FEATURE_LEVEL_11_0,
+	};
+
+#ifdef _DEBUG
+	auto result = CreateDXGIFactory2(DXGI_CREATE_FACTORY_DEBUG, IID_PPV_ARGS(&_dxgiFactory));
+#else
+	auto result = CreateDXGIFactory1(IID_PPV_ARGS(&_dxgiFactory));
+#endif // _DEBUG
+
+
+	// アダプター列挙用
+	std::vector <IDXGIAdapter*> adapters;
+	// ここに特定の名前を持つアダプターオブジェクトが入る
+	IDXGIAdapter* tmpAdapter = nullptr;
+	for (int i = 0;
+		_dxgiFactory->EnumAdapters(i, &tmpAdapter) != DXGI_ERROR_NOT_FOUND;
+		++i)
+	{
+		adapters.push_back(tmpAdapter);
+	}
+	for (auto adpt : adapters)
+	{
+		DXGI_ADAPTER_DESC adesc = {};
+		adpt->GetDesc(&adesc); // アダプターの説明オブジェクト取得
+		std::wstring strDesc = adesc.Description;
+		// 探したいアダプターの名前を確認
+		if (strDesc.find(L"NVIDIA") != std::string::npos)
+		{
+			tmpAdapter = adpt;
+			break;
+		}
+	}
+
+	// Direct3D デバイスの初期化
+	D3D_FEATURE_LEVEL featureLevel;
+	for (auto lv : levels) // アダプターを指定
+	{
+		if (D3D12CreateDevice(tmpAdapter, lv, IID_PPV_ARGS(&_dev)) == S_OK)
+		{
+			featureLevel = lv;
+			break; // 生成可能なバージョンが見つかったらループを打ち切り
+		}
+	}
+
+	result = _dev->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT,
+		IID_PPV_ARGS(&_cmdAllocator));
+	result = _dev->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_DIRECT,
+		_cmdAllocator, nullptr,
+		IID_PPV_ARGS(&_cmdList));
+
+	D3D12_COMMAND_QUEUE_DESC cmdQueueDesc = {};
+	// タイムアウトなし
+	cmdQueueDesc.Flags = D3D12_COMMAND_QUEUE_FLAG_NONE;
+	// アダプターを1つしか使わないときは0でよい
+	cmdQueueDesc.NodeMask = 0;
+	//プライオリティは時に指定なし
+	cmdQueueDesc.Priority = D3D12_COMMAND_QUEUE_PRIORITY_NORMAL;
+	//コマンドリストと合わせる
+	cmdQueueDesc.Type = D3D12_COMMAND_LIST_TYPE_DIRECT;
+	//キュー生成
+	result = _dev->CreateCommandQueue(&cmdQueueDesc, IID_PPV_ARGS(&_cmdQueue));
+
+	HRESULT CreateSwapChainForHwnd(
+		IUnknown * pDevice,
+		HWND hwnd,
+		const DXGI_SWAP_CHAIN_DESC1 * pDesc,
+		const DXGI_SWAP_CHAIN_FULLSCREEN_DESC * pFullscreenDesc,
+		IDXGIOutput * pRestrictToOutput,
+		IDXGISwapChain1 * *ppSwapChain
+	);
+
+	DXGI_SWAP_CHAIN_DESC1 swapchainDesc = {};
+	swapchainDesc.Width = window_width;
+	swapchainDesc.Height = window_height;
+	swapchainDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	swapchainDesc.Stereo = false;
+	swapchainDesc.SampleDesc.Count = 1;
+	swapchainDesc.SampleDesc.Quality = 0;
+	swapchainDesc.BufferUsage = DXGI_USAGE_BACK_BUFFER;
+	swapchainDesc.BufferCount = 2;
+	// バックバッファーは伸び縮み可能
+	swapchainDesc.Scaling = DXGI_SCALING_STRETCH;
+	// フリップ後は速やかに破棄
+	swapchainDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
+	// 特に指定なし
+	swapchainDesc.AlphaMode = DXGI_ALPHA_MODE_UNSPECIFIED;
+	// ウィンドウ⇔フルスクリーン切り替え可能
+	swapchainDesc.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;
+	result = _dxgiFactory->CreateSwapChainForHwnd(
+		_cmdQueue, hwnd,
+		&swapchainDesc, nullptr, nullptr,
+		(IDXGISwapChain1**)&_swapchain); // 本来はQueryInterface などを用いてIDXGISChain4* への変換チェックをする
+
+	D3D12_DESCRIPTOR_HEAP_DESC heapDesc = {};
+	heapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;
+	heapDesc.NodeMask = 0;
+	heapDesc.NumDescriptors = 2;
+	heapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
+
+	ID3D12DescriptorHeap* rtvHeaps = nullptr;
+	result = _dev->CreateDescriptorHeap(&heapDesc, IID_PPV_ARGS(&rtvHeaps));
+
+	DXGI_SWAP_CHAIN_DESC swcDesc = {};
+	result = _swapchain->GetDesc(&swcDesc);
+
+	std::vector<ID3D12Resource*> _backBuffers(swcDesc.BufferCount);
+	for (int idx = 0; idx < swcDesc.BufferCount; ++idx)
+	{
+		result = _swapchain->GetBuffer(idx, IID_PPV_ARGS(&_backBuffers[idx]));
+		D3D12_CPU_DESCRIPTOR_HANDLE handle
+			= rtvHeaps->GetCPUDescriptorHandleForHeapStart();
+		handle.ptr += idx * _dev->GetDescriptorHandleIncrementSize(
+			D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
+		_dev->CreateRenderTargetView(_backBuffers[idx], nullptr, handle);
+	}
+
+	ID3D12Fence* _fence = nullptr;
+	UINT64 _fenceVal = 0;
+	result = _dev->CreateFence(_fenceVal, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&_fence));
+
+	ShowWindow(hwnd, SW_SHOW);
+
+	XMFLOAT3 vertices[] = {
+		{-1.0f, -1.0f, 0.0f}, //左下
+		{-1.0f, +1.0f, 0.0f}, //左上
+		{+1.0f, -1.0f, 0.0f}, //右下
+	};
+
+	D3D12_HEAP_PROPERTIES heapprop = {};
+	heapprop.Type = D3D12_HEAP_TYPE_UPLOAD;
+	heapprop.CPUPageProperty = D3D12_CPU_PAGE_PROPERTY_UNKNOWN;
+	heapprop.MemoryPoolPreference = D3D12_MEMORY_POOL_UNKNOWN;
+
+	D3D12_RESOURCE_DESC resdesc = {};
+	resdesc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;
+	resdesc.Width = sizeof(vertices); // 頂点情報が入るだけのサイズ
+	resdesc.Height = 1;
+	resdesc.DepthOrArraySize = 1;
+	resdesc.MipLevels = 1;
+	resdesc.Format = DXGI_FORMAT_UNKNOWN;
+	resdesc.SampleDesc.Count = 1;
+	resdesc.Flags = D3D12_RESOURCE_FLAG_NONE;
+	resdesc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
+
+	ID3D12Resource* vertBuff = nullptr;
+
+	result = _dev->CreateCommittedResource(
+		&heapprop,
+		D3D12_HEAP_FLAG_NONE,
+		&resdesc,
+		D3D12_RESOURCE_STATE_GENERIC_READ,
+		nullptr,
+		IID_PPV_ARGS(&vertBuff));
+
+	XMFLOAT3* vertMap = nullptr;
+	result = vertBuff->Map(0, nullptr, (void**)&vertMap);
+	std::copy(std::begin(vertices), std::end(vertices), vertMap);
+	vertBuff->Unmap(0, nullptr);
+
+	D3D12_VERTEX_BUFFER_VIEW vbView = {};
+	vbView.BufferLocation = vertBuff->GetGPUVirtualAddress(); // バッファーの仮想アドレス
+	vbView.SizeInBytes = sizeof(vertices); // 全バイト数
+	vbView.StrideInBytes = sizeof(vertices[0]); // 1頂点あたりのバイト数
+
+	//UnregisterClass(w.lpszClassName, w.hInstance);
+
+	ID3DBlob* _vsBlob = nullptr;
+	ID3DBlob* _psBlob = nullptr;
+
+	ID3DBlob* errorBlob = nullptr;
+	result = D3DCompileFromFile(
+		L"BasicVertexShader.hlsl",
+		nullptr,
+		D3D_COMPILE_STANDARD_FILE_INCLUDE,
+		"BasicVS", "vs_5_0",
+		D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION,
+		0,
+		&_vsBlob, &errorBlob);
+	if (FAILED(result)) {
+		if (result == HRESULT_FROM_WIN32(ERROR_FILE_NOT_FOUND)) {
+			::OutputDebugStringA("ファイルが見当たりません");
+		}    //デバッグウィンドウへのエラー表示
+		else {
+			std::string errstr;
+			errstr.resize(errorBlob->GetBufferSize());
+			std::copy_n((char*)errorBlob->GetBufferPointer(),
+				errorBlob->GetBufferSize(), errstr.begin());
+			errstr += "\n";
+			OutputDebugStringA(errstr.c_str());
+		}
+		exit(1);
+	}
+
+	result = D3DCompileFromFile(
+		L"BasicPixelShader.hlsl",
+		nullptr,
+		D3D_COMPILE_STANDARD_FILE_INCLUDE,
+		"BasicPS", "ps_5_0",
+		D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION,
+		0,
+		&_psBlob, &errorBlob);
+	if (FAILED(result)) {
+		if (result == HRESULT_FROM_WIN32(ERROR_FILE_NOT_FOUND)) {
+			::OutputDebugStringA("ファイルが見当たりません");
+		}
+		else {
+			std::string errstr;
+			errstr.resize(errorBlob->GetBufferSize());
+			std::copy_n((char*)errorBlob->GetBufferPointer(),
+				errorBlob->GetBufferSize(), errstr.begin());
+			errstr += "\n";
+			OutputDebugStringA(errstr.c_str());
+		}
+		exit(1);
+	}
+
+	D3D12_INPUT_ELEMENT_DESC inputLayout[] =
+	{
+		{"POSITION" , 0, DXGI_FORMAT_R32G32B32_FLOAT, 0,
+		D3D12_APPEND_ALIGNED_ELEMENT,
+		D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA,0},
+	};
+
+	D3D12_GRAPHICS_PIPELINE_STATE_DESC gpipeline = {};
+
+	gpipeline.pRootSignature = nullptr;
+
+	gpipeline.VS.pShaderBytecode = _vsBlob->GetBufferPointer();
+	gpipeline.VS.BytecodeLength = _vsBlob->GetBufferSize();
+	gpipeline.PS.pShaderBytecode = _psBlob->GetBufferPointer();
+	gpipeline.PS.BytecodeLength = _psBlob->GetBufferSize();
+
+	while (true)
+	{
+		MSG msg;
+		if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE)) {}
+		{
+			//DirectX処理
+			//バックバッファのインデックスを取得
+			auto bbIdx = _swapchain->GetCurrentBackBufferIndex();
+
+			D3D12_RESOURCE_BARRIER BarrierDesc = {};
+			BarrierDesc.Type = D3D12_RESOURCE_BARRIER_TYPE_TRANSITION;
+			BarrierDesc.Flags = D3D12_RESOURCE_BARRIER_FLAG_NONE;
+			BarrierDesc.Transition.pResource = _backBuffers[bbIdx];
+			BarrierDesc.Transition.Subresource = 0;
+			BarrierDesc.Transition.StateBefore = D3D12_RESOURCE_STATE_PRESENT;
+			BarrierDesc.Transition.StateAfter = D3D12_RESOURCE_STATE_RENDER_TARGET;
+			_cmdList->ResourceBarrier(1, &BarrierDesc);
+
+			//レンダーターゲットを指定
+			auto rtvH = rtvHeaps->GetCPUDescriptorHandleForHeapStart();
+			rtvH.ptr += bbIdx * _dev->GetDescriptorHandleIncrementSize(
+				D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
+			_cmdList->OMSetRenderTargets(1, &rtvH, true, nullptr);
+
+			// 画面クリア
+			FlameTime++;
+			float Color = 0;
+			if (FlameTime % 2 == 1) {
+				Color = 1.0f;
+			}
+			else {
+				Color = 0.0f;
+			}
+
+			float clearColor[] = { 1.0f, 1.0f, Color, 1.0f };
+			_cmdList->ClearRenderTargetView(rtvH, clearColor, 0, nullptr);
+
+			// 前後に入れ替える
+			BarrierDesc.Transition.StateBefore = D3D12_RESOURCE_STATE_RENDER_TARGET;
+			BarrierDesc.Transition.StateAfter = D3D12_RESOURCE_STATE_PRESENT;
+			_cmdList->ResourceBarrier(1, &BarrierDesc);
+
+			// 命令のクローズ
+			_cmdList->Close();
+
+			// コマンドリストの実行
+			ID3D12CommandList* cmdlists[] = { _cmdList };
+			_cmdQueue->ExecuteCommandLists(1, cmdlists);
+
+			/// 待ち
+			_cmdQueue->Signal(_fence, ++_fenceVal);
+
+			if (_fence->GetCompletedValue() != _fenceVal) {
+				auto event = CreateEvent(nullptr, false, false, nullptr);
+				_fence->SetEventOnCompletion(_fenceVal, event);
+				WaitForSingleObject(event, INFINITE);
+				CloseHandle(event);
+			}
+
+			_cmdAllocator->Reset(); // キュークリア
+			_cmdList->Reset(_cmdAllocator, nullptr); // 再びコマンドリストをためる準備
+
+			// フリップ
+			_swapchain->Present(1, 0);
+		}
+
+		if (msg.message == WM_QUIT)
+		{
+			break;
+		}
+	}
+
+	UnregisterClass(w.lpszClassName, w.hInstance);
+
+	return 0;
+}
+#endif
+
+//struct XMFLOAT3 
+//{
+//	float x;
+//	float y;
+//	float z;
+//
+//	XMFLOAT3() = default;
+//
+//	XMFLOAT3(const XMFLOAT3&) = default;
+//	XMFLOAT3& operator=(const XMFLOAT3&) = default;
+//
+//	XMFLOAT3(XMFLOAT3&) = default;
+//	XMFLOAT3& operator=(XMFLOAT3&) = default;
+//
+//	constexpr XMFLOAT3(float _x, float _y, float _z) noexcept : x(_x), y(_y), z(_z) {}
+//	explicit XMFLOAT3(_In_reads_(3) const float* pArray) noexcept : x(pArray[0]), y(pArray[1]), z(pArray[2]) {}
+//};
