eef100ec36cbdcd737415475da8edddcbb58153e
isigurohatune <71632467+isigurohatune@users.noreply.github.com>
2022-10-02 09:50:36 +0900

    Add files via upload

--- PGWS4/PGWS4/main.cpp
+++ PGWS4/PGWS4/main.cpp
@@ -2,14 +2,18 @@
 #include<tchar.h>
 #include<d3d12.h>
 #include<dxgi1_6.h>
+#include<DirectXMath.h>
 #include<vector>
+#include<d3dcompiler.h>
 #ifdef _DEBUG
 #include<iostream>
 #endif
 
 #pragma comment(lib,"d3d12.lib")
 #pragma comment(lib,"dxgi.lib")
+#pragma comment(lib,"d3dcompiler.lib")
 using namespace std;
+using namespace DirectX;
 
 void DebugOutPutFormatStaring(const char* format, ...)
 {
@@ -31,6 +35,18 @@ LRESULT WindowProcedure(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
 	}
 	return DefWindowProc(hwnd, msg, wparam, lparam);//既定の処理を行う
 }
+
+#ifdef _DEBUG
+void EnableDebugLayer()
+{
+	ID3D12Debug* debugLayer = nullptr;
+	HRESULT result = D3D12GetDebugInterface(IID_PPV_ARGS(&debugLayer));
+	if (!SUCCEEDED(result))return;
+
+	debugLayer->EnableDebugLayer(); //デバッグレイヤーを有効にする
+	debugLayer->Release();          //有効化したらインターフェイスを開放する
+}
+#endif _DEBUG
 
 #ifdef _DEBUG
 int main()
@@ -76,6 +92,11 @@ int WINAOI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 		w.hInstance,                          //呼び出しアプリケーション
 		nullptr);                             //追加パラメーター
 
+#ifdef  _DEBUG
+	//デバッグレイヤーをオンに
+	EnableDebugLayer();
+#endif //  _DEBUG
+
 	D3D_FEATURE_LEVEL levels[] =
 	{
 		D3D_FEATURE_LEVEL_12_1,
@@ -84,7 +105,12 @@ int WINAOI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 		D3D_FEATURE_LEVEL_11_0,
 	};
 
+#ifdef _DEBUG
+	auto result = CreateDXGIFactory2(DXGI_CREATE_FACTORY_DEBUG, IID_PPV_ARGS(&_dxgiFactory));
+#else
 	auto result = CreateDXGIFactory1(IID_PPV_ARGS(&_dxgiFactory));
+#endif // _DEBUG
+
 	//アダプターの列挙用
 	std::vector<IDXGIAdapter*>adapters;
 	//ここに特定の名前を持つアダプターオブジェクトが入る
@@ -179,8 +205,94 @@ int WINAOI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 			D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
 		_dev->CreateRenderTargetView(_backBuffers[idx], nullptr, handle);
     }
+
+	ID3D12Fence* _fence = nullptr;
+	UINT64 _fenceVal = 0;
+	result = _dev->CreateFence(_fenceVal, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&_fence));
+
 	//ウィンドウ表示
 	ShowWindow(hwnd, SW_SHOW);
+
+	XMFLOAT3 vertices[] = {
+		{-1.0f,-1.0f,0.0f},  //左下
+		{-1.0f, +1.0f,0.0f },  //左上
+		{ 1.0f,-1.0f,0.0f },  //右下
+	};
+
+	D3D12_HEAP_PROPERTIES heapprop = {};
+	heapprop.Type = D3D12_HEAP_TYPE_UPLOAD;
+	heapprop.CPUPageProperty = D3D12_CPU_PAGE_PROPERTY_UNKNOWN;
+	heapprop.MemoryPoolPreference = D3D12_MEMORY_POOL_UNKNOWN;
+
+	D3D12_RESOURCE_DESC resdesc = {};
+	resdesc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;
+	resdesc.Width = sizeof(vertices);  //頂点情報が入るだけのサイズ
+	resdesc.Height = 1;
+	resdesc.DepthOrArraySize = 1;
+	resdesc.MipLevels = 1;
+	resdesc.Format = DXGI_FORMAT_UNKNOWN;
+	resdesc.SampleDesc.Count = 1;
+	resdesc.Flags = D3D12_RESOURCE_FLAG_NONE;
+	resdesc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
+
+	ID3D12Resource* vertBuff = nullptr;
+
+	result = _dev->CreateCommittedResource(
+		&heapprop,
+		D3D12_HEAP_FLAG_NONE,
+		&resdesc,
+		D3D12_RESOURCE_STATE_GENERIC_READ,
+		nullptr,
+		IID_PPV_ARGS(&vertBuff));
+
+	XMFLOAT3* vertMap = nullptr;
+	result = vertBuff->Map(0, nullptr, (void**)&vertMap);
+	std::copy(std::begin(vertices), std::end(vertices), vertMap);
+	vertBuff->Unmap(0, nullptr);
+
+	D3D12_VERTEX_BUFFER_VIEW vbView = {};
+	vbView.BufferLocation = vertBuff->GetGPUVirtualAddress();  //バッファーの仮想アドレス
+	vbView.SizeInBytes = sizeof(vertices);  //全バイト数
+	vbView.StrideInBytes = sizeof(vertices[0]); //1頂点あたりのバイト数
+
+	ID3DBlob* _vsBlob = nullptr;
+	ID3DBlob* _psBlob = nullptr;
+
+	ID3DBlob* errorBlob = nullptr;
+	result = D3DCompileFromFile(
+		L"BasicVertexShader.hlsl",  //シェーダー名
+		nullptr,  //defineはなし
+		D3D_COMPILE_STANDARD_FILE_INCLUDE,  //インクルードはデフォルト
+		"BasicVS","vs_5_0",  //関数はBasicVS,対象シェーダーはvs_5_0
+		D3DCOMPILE_DEBUG|D3DCOMPILE_SKIP_OPTIMIZATION,  //デバッグ用および最適化なし
+		0,
+		&_vsBlob,&errorBlob);  //エラー時はerrorBlobにメッセージが入る
+	if (FAILED(result))
+	{
+		if (result == HRESULT_FROM_WIN32(ERROR_FILE_NOT_FOUND))
+		{
+			::OutputDebugStringA("ファイルが見当たりません");
+		}
+		else
+		{
+			std::string errstr;
+			errstr.resize(errorBlob->GetBufferSize());
+			std::copy_n((char*)errorBlob->GetBufferPointer(),
+				errorBlob->GetBufferSize(), errstr.begin());
+			errstr += "\n";
+			OutputDebugStringA(errstr.c_str());
+		}
+		exit(1);
+	}
+
+	D3D12_INPUT_ELEMENT_DESC inputLayout[] =
+	{
+		{"POSITION",0,DXGI_FORMAT_R32G32B32A32_FLOAT,0,
+		D3D12_APPEND_ALIGNED_ELEMENT,
+		D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA,0},
+	};
+
+	float alph = 0;
 	while (true)
 	{
 		MSG msg ;
@@ -198,15 +310,36 @@ int WINAOI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 		//バックバッファのインデックスを取得
 		auto bbIdx = _swapchain->GetCurrentBackBufferIndex();
 
+		D3D12_RESOURCE_BARRIER BarrierDesc = {};
+		BarrierDesc.Type = D3D12_RESOURCE_BARRIER_TYPE_TRANSITION; //遷移
+		BarrierDesc.Flags = D3D12_RESOURCE_BARRIER_FLAG_NONE;  //特になし
+		BarrierDesc.Transition.pResource = _backBuffers[bbIdx];  //バックバッファ―リソース
+		BarrierDesc.Transition.Subresource = 0;
+		BarrierDesc.Transition.StateBefore = D3D12_RESOURCE_STATE_PRESENT;  //直前はPRESENT状態
+		BarrierDesc.Transition.StateAfter = D3D12_RESOURCE_STATE_RENDER_TARGET;  //今からRT状態
+		_cmdList->ResourceBarrier(1, &BarrierDesc);  //バリア指定実行
+
+
 		//レンダーターゲットを指定
 		auto rtvH = rtvHeaps->GetCPUDescriptorHandleForHeapStart();
 		rtvH.ptr += bbIdx * _dev->GetDescriptorHandleIncrementSize(
 			D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
-		_cmdList->OMSetRenderTargets(1, &rtvH, 0,nullptr);
+		_cmdList->OMSetRenderTargets(0, &rtvH, 0,nullptr);
 
 		//画面クリア
-		float clearColor[] = { 1.0f,1.0f,0.0f,1.0f };
+		alph = alph + 0.01f;
+		if (alph > 1.0f)
+		{
+			alph = 0.0f;
+		}
+		float clearColor[] = { 1.0f,1.0f,alph,1.0f };
 		_cmdList->ClearRenderTargetView(rtvH, clearColor, 0, nullptr);
+
+
+		//前後だけ入れ替える
+		BarrierDesc.Transition.StateBefore = D3D12_RESOURCE_STATE_RENDER_TARGET;
+		BarrierDesc.Transition.StateAfter = D3D12_RESOURCE_STATE_PRESENT;
+		_cmdList->ResourceBarrier(1, &BarrierDesc);
 
 		//命令のクローズ
 		_cmdList->Close();
@@ -214,6 +347,18 @@ int WINAOI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 		//コマンドリストの実行
 		ID3D12CommandList* cmdlists[] = { _cmdList };
 		_cmdQeue->ExecuteCommandLists(1, cmdlists);
+		//_swapchain->Present(1, 0);
+
+		//待ち
+		_cmdQeue->Signal(_fence, ++_fenceVal);
+
+		if (_fence->GetCompletedValue() != _fenceVal)
+		{
+			auto event = CreateEvent(nullptr, false, false, nullptr);
+			_fence->SetEventOnCompletion(_fenceVal, event);  //イベントハンドルの取得
+			WaitForSingleObject(event, INFINITE);  //イベントが発生するまで無限に待つ
+			CloseHandle(event);  //イベントハンドルを閉じる
+		}
 
 		_cmdList->Reset(_cmdAllocator, nullptr); //キューをクリア
 		_swapchain->Present(1, 0); //再びコマンドリストを溜める準備
