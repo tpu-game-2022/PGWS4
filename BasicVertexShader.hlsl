9729d9e6be058f4fec6e317808fb1f9eefa91507
sanai371 <71958015+sanai371@users.noreply.github.com>
2022-10-17 19:34:16 +0900

    第4回チャレンジ課題の追加

--- BasicVertexShader.hlsl
+++ BasicVertexShader.hlsl
@@ -1,4 +1,11 @@
-float4 BasicVS( float4 pos : POSITION ) : SV_POSITION
-{
-	return pos;
+struct Output {
+	float4 pos:POSITION;
+	float4 svpos:SV_POSITION;
+};
+
+Output BasicVS(float4 pos : POSITION) {
+	Output output;
+	output.pos = pos;
+	output.svpos = pos;
+	return output;
 }
