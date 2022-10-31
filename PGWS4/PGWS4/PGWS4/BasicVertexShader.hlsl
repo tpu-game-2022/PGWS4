afa88a6c89284f0d970e63af754794a4f83cf252
nazonokouta0706 <71791660+nazonokouta0706@users.noreply.github.com>
2022-10-31 10:11:51 +0900

    Add files via upload

---
+++ PGWS4/PGWS4/PGWS4/BasicVertexShader.hlsl
@@ -0,0 +1,15 @@
+#include "BasicShaderHeader.hlsli"
+
+//
+//struct Output {
+//	float4 pos:POSITION;
+//	float4 svpos:SV_POSITION;
+//};
+
+Output BasicVS(float4 pos : POSITION, float2 uv : TEXCOORD)
+{
+	Output output;//ピクセルシェーダーに渡す値
+	output.svpos = pos;
+	output.uv = uv;
+	return output;
+}
