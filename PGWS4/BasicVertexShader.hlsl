7e7a9182410cf83e59ab13a35d38b59648b56418
HosakaKota <71623933+HosakaKota@users.noreply.github.com>
2022-11-15 18:59:32 +0900

    Update BasicVertexShader.hlsl

--- PGWS4/BasicVertexShader.hlsl
+++ PGWS4/BasicVertexShader.hlsl
@@ -1,9 +1,15 @@
 #include "BasicShaderHeader.hlsli"
 
-Output BasicVS(float4 pos : POSITION, float2 uv : TEXCOORD)
+Output BasicVS(
+	float4 pos : POSITION, 
+	float4 normal : NORMAL,
+	float2 uv : TEXCOORD,
+	min16uint2 boneno : BONE_NO,
+	min16uint weight : WEIGHT)
 {
-	Output output; // ピクセルシェーダーに渡す値
-	output.svpos = pos;
+	Output output; // 繝斐け繧ｻ繝ｫ繧ｷ繧ｧ繝ｼ繝繝ｼ縺ｫ貂｡縺吝､
+	output.svpos = mul(mat, pos);
+	output.normal = normal;
 	output.uv = uv;
 	return output;
 }
