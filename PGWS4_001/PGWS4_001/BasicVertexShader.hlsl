8f092f5ce193f56a518502b6ba9d983a573da0f7
g2027006 <71998147+g2027006@users.noreply.github.com>
2022-12-12 04:07:21 +0900

    Add files via upload

---
+++ PGWS4_001/PGWS4_001/BasicVertexShader.hlsl
@@ -0,0 +1,14 @@
+#include "BasicShaderHeader.hlsli"
+
+Output BasicVS(float4 pos : POSITION, float4 normal : NORMAL, float2 uv : TEXCOORD, min16uint2 boneno : BONE_NO, min16uint weight : WEIGHT)
+{
+	Output output;
+	output.svpos = mul(proj, mul(view, mul(world, pos)));
+	normal.w = 0;
+	output.normal = mul(world, normal);
+	output.vnormal = mul(view, output.normal);
+	output.uv = uv;
+	pos = mul(world, pos);
+	output.ray = pos.xyz - eye;
+	return output;
+}
