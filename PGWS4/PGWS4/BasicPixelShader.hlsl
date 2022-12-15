13ec999367137773670acc1792a732b7209fec96
isigurohatune <71632467+isigurohatune@users.noreply.github.com>
2022-12-15 12:53:48 +0900

    Update BasicPixelShader.hlsl

--- PGWS4/PGWS4/BasicPixelShader.hlsl
+++ PGWS4/PGWS4/BasicPixelShader.hlsl
@@ -1,9 +1,11 @@
 #include "BasicShaderHeader.hlsli"
 
+#define TOON
+
 float4 BasicPS(Output input) : SV_TARGET
 {
 	float3 light = normalize(float3(1,-1,1));  //平行光線ベクトル
-	float3 lightColor = float3(1.0, 1.0, 1.0) * 0.7;
+	float3 lightColor = float3(0.7, 0.7, 0.7);
 	float brightness = dot(-light, input.normal.xyz);
 	float3 refLight = normalize(reflect(light, normalize(input.normal.xyz)));
 	float specularB = pow(saturate(dot(refLight, -normalize(input.ray))), specular.a);
@@ -13,11 +15,22 @@ float4 BasicPS(Output input) : SV_TARGET
 	float2 sphereMapUV = input.vnormal.xy * float2(0.5, -0.5) + 0.5;
 	float4 color = tex.Sample(smp, input.uv);
 	float4 texColor = tex.Sample(smp, input.uv);
+
+#ifdef TOON
+		float3 toonDif = toon.Sample(smpToon, float2(0, 1.0 - diffuseB)).rgb;
+		return float4(lightColor *// ライトカラー
+			(texColor.rgb // テクスチャカラー
+				* sph.Sample(smp, sphereMapUV).rgb // スフィアマップ（乗算）
+				* (ambient + toonDif * diffuse.rgb) // 環境光＋ディフューズ色
+				+ spa.Sample(smp, sphereMapUV).rgb// スフィアマップ（加算）
+				+ specularB * specular.rgb) // スペキュラ
+			, diffuse.a); // アルファ
+#endif // TOON
 	return float4(lightColor * //ライトカラー
 		(texColor.rgb  //テクスチャカラー
 			* sph.Sample(smp, sphereMapUV).rgb  //スフィアマップ（乗算）
 			* (ambient + diffuseB * diffuse.rgb)  //環境光＋出フューズ色
 			+ spa.Sample(smp, sphereMapUV).rgb  //スフィアマップ（加算）
-			+ specularB * specular.rgb)  //スペキュラ
+			+ specularB)  //スペキュラ
 		, diffuse.a);  //アルファ
 }
