c7ce3fd7ab2540462896d1627f215b1c96b7a598
HosakaKota <71623933+HosakaKota@users.noreply.github.com>
2022-12-19 21:16:48 +0900

    Update BasicPixelShader.hlsl

--- PGWS4/BasicPixelShader.hlsl
+++ PGWS4/BasicPixelShader.hlsl
@@ -1,12 +1,14 @@
 #include "BasicShaderHeader.hlsli"
 
+#define TOON
+
 float4 BasicPS(Output input) : SV_TARGET
 {
 	//光の向かうベクトル(平行光線ベクトル)
 	float3 light = normalize(float3(1,-1,1));
 
 	//ライトカラー(1,1,1で真っ白)
-	float3 lightColor = float3(1.0, 1.0, 1.0);
+	float3 lightColor = float3(0.7, 0.7, 0.7);
 
 	//ディフューズ計算
 	float3 normal = normalize(input.normal.xyz);
@@ -20,11 +22,17 @@ float4 BasicPS(Output input) : SV_TARGET
 
 	//テクスチャカラー
 	float4 texColor = tex.Sample(smp, input.uv);
+
+#ifdef TOON
+	float3 toonDif = toon.Sample(smpToon, float2(0, 1.0 - diffuseB)).rgb;
+	float3 toonSpc = toon.Sample(smpToon, float2(0, 1.0 - specularB)).rgb;
 	return float4(lightColor *//ライトカラー
 		(texColor.rgb//テクスチャカラー
 			* sph.Sample(smp, sphereMapUV).rgb//スフィアマップ(乗算)
-			* (ambient + diffuseB * diffuse.rgb)//環境光＋ディフューズ色
+			* (ambient + toonDif * diffuse.rgb)//環境光＋ディフューズ色
 			+ spa.Sample(smp, sphereMapUV).rgb//スフィアマップ(加算)
-			+ specularB * specular.rgb)//スぺキュラ
+			//+ specularB * specular.rgb)//スぺキュラ
+			+ toonSpc * specular.rgb)
 		, diffuse.a);//アルファ
+#endif //TOON
 }
