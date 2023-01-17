6dcc5e3758099cb21b78340d6ac7931cd7c3e087
noguaya <Ayaneko4180@gmail.com>
2023-01-18 05:58:12 +0900

    第14回目までの分です

---
+++ PGWS4/PGWS4/Application.cpp
@@ -0,0 +1,145 @@
+﻿#include "Application.h"
+#include <tchar.h>
+#include "Dx12Wrapper.h"
+#include "PMDRenderer.h"
+#include "PMDActor.h"
+
+using namespace DirectX;
+using namespace Microsoft::WRL;
+
+// @brief コンソール画面にフォーマット付き文字列を表示
+// @param format フォーマット（%d とか %f とかの）
+// @param 可変長引数
+// @remarks この関数はデバッグ用です。デバッグ時にしか動作しません
+static void DebugOutputFormatString(const char* format, ...)
+{
+#ifdef _DEBUG
+	va_list valist;
+	va_start(valist, format);
+	vprintf_s(format, valist);
+	va_end(valist);
+#endif
+}
+
+static inline void ThrowIfFailed(HRESULT hr)
+{
+	if (FAILED(hr))
+	{
+		throw std::exception();
+	}
+}
+
+// 面倒だけど書かなければいけない関数
+static LRESULT WindowProcedure(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
+{
+	// ウィンドウが破棄されたら呼ばれる
+	if (msg == WM_DESTROY)
+	{
+		PostQuitMessage(0); // OS に対して「もうこのアプリは終わる」と伝える
+		return 0;
+	}
+	return DefWindowProc(hwnd, msg, wparam, lparam); // 既定の処理を行う
+}
+
+
+static HWND CreateGameWindow(WNDCLASSEX& windowClass, UINT width, UINT height)
+{
+	windowClass.cbSize = sizeof(WNDCLASSEX);
+	windowClass.lpfnWndProc = (WNDPROC)WindowProcedure; // コールバック関数の指定
+	windowClass.lpszClassName = _T("DX12Sample"); // アプリケーションクラス名（適当でよい）
+	windowClass.hInstance = GetModuleHandle(nullptr); // ハンドルの取得
+
+	RegisterClassEx(&windowClass); // アプリケーションクラス（ウィンドウクラスの指定を OS に伝える）
+
+	RECT wrc = { 0, 0, (LONG)width, (LONG)height }; // ウィンドウサイズを決める
+
+	// 関数を使ってウィンドウのサイズを補正する
+	AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
+	// ウィンドウオブジェクトの生成
+	return CreateWindow(windowClass.lpszClassName, // クラス名指定
+		_T("DX12 テスト "), // タイトルバーの文字
+		WS_OVERLAPPEDWINDOW, // タイトルバーと境界線があるウィンドウ
+		CW_USEDEFAULT, // 表示 x 座標は OS にお任せ
+		CW_USEDEFAULT, // 表示 y 座標は OS にお任せ
+		wrc.right - wrc.left, // ウィンドウ幅
+		wrc.bottom - wrc.top, // ウィンドウ高
+		nullptr, // 親ウィンドウハンドル
+		nullptr, // メニューハンドル
+		windowClass.hInstance, // 呼び出しアプリケーションハンドル
+		nullptr); // 追加パラメーター
+}
+
+
+bool Application::Init()
+{
+	ThrowIfFailed(CoInitializeEx(0, COINIT_MULTITHREADED));// 呼ばないと動作しないメソッドがある
+
+	_hwnd = CreateGameWindow(_windowClass, _window_width, _window_height);// ウィンドウ生成
+
+	_dx12.reset(new Dx12Wrapper(_hwnd, _window_width, _window_height));
+	_pmdRenderer.reset(new PMDRenderer(*_dx12));
+
+	//	_pmdActor.reset(new PMDActor("Model/巡音ルカ.pmd", *_pmdRenderer));
+	//	_pmdActor.reset(new PMDActor("Model/初音ミクmetal.pmd", *_pmdRenderer));
+	_pmdActor.reset(new PMDActor("Model/初音ミク.pmd", *_pmdRenderer));
+
+	return true;
+}
+
+void Application::Run()
+{
+	//ウィンドウ表示
+	ShowWindow(_hwnd, SW_SHOW);
+
+	while (true)
+	{
+		MSG msg;
+		if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+		{
+			// アプリケーションが終わるときに message が WM_QUIT になる
+			if (msg.message == WM_QUIT)
+			{
+				break;
+			}
+
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+
+		_pmdActor->Update();
+
+		_dx12->BeginDraw();
+
+		auto _cmdList = _dx12->CommandList();
+		_cmdList->SetPipelineState(_pmdRenderer->GetPipelineState());
+		_cmdList->SetGraphicsRootSignature(_pmdRenderer->GetRootSignature());
+
+		_dx12->ApplySceneDescHeap();
+
+		_cmdList->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+		_pmdActor->Draw();
+
+		_dx12->EndDraw();
+	}
+}
+
+void Application::Terminate()
+{
+	//もうクラス使わんから登録解除
+	UnregisterClass(_windowClass.lpszClassName, _windowClass.hInstance);
+}
+
+Application::Application()
+{
+}
+
+
+Application::~Application()
+{
+}
+
+Application& Application::Instance()
+{
+	static Application instance;
+	return instance;
+}
--- PGWS4/PGWS4/BasicShaderHeader.hlsli
+++ PGWS4/PGWS4/BasicShaderHeader.hlsli
@@ -1,13 +1,39 @@
+// 頂点シェーダーからピクセルシェーダーへのやり取りに使用する構造体
 struct Output
 {
-	float4 svpos:SV_POSITION;//システム用頂点座標
-	float2 uv:TEXCOORD;//uv値
+	float4 svpos : SV_POSITION; // システム用頂点座標
+	float4 normal : NORMAL0; // 法線ベクトル
+	float4 vnormal : NORMAL1; // ビュー変換後の法線ベクトル
+	float2 uv :TEXCOORD; // uv 値
+	float3 ray : VECTOR; // ベクトル
 };
 
-Texture2D<float4> tex:register(t0);	//0番スロットに設定されたテクスチャ
-SamplerState smp :register(s0);		//0番スロットに設定されたサンプラー
+Texture2D<float4> tex : register(t0); // 0 番スロットに設定されたテクスチャ
+Texture2D<float4> sph : register(t1); // 1 番スロットに設定されたテクスチャ
+Texture2D<float4> spa : register(t2); // 2 番スロットに設定されたテクスチャ
+Texture2D<float4> toon : register(t3); // 3 番スロットに設定されたテクスチャ（トゥーン）
+SamplerState smp : register(s0); // 0 番スロットに設定されたサンプラー
+SamplerState smpToon : register(s1); // 1 番スロットに設定されたサンプラー（トゥーン用）
 
-cbuffer cbuff0 : register(b0)
+// 定数バッファー
+cbuffer SceneData : register(b0)
 {
-	matrix mat;
+	matrix view; // ビュー行列
+	matrix proj; // プロジェクション行列
+	float3 eye; // 視点
+};
+
+cbuffer Transform : register(b1)
+{
+	matrix world; // ワールド行列
+	matrix bones[256]; //ボーン行列
+};
+
+// 定数バッファー2
+// マテリアル用
+cbuffer Material : register(b2)
+{
+	float4 diffuse;	// ディフューズ色
+	float4 specular;// スペキュラ
+	float3 ambient;	// アンビエント
 };
